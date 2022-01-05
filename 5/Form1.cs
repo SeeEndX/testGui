069845cd37cfdecfebfef5782b3a536886562107
Арсений Сендецкий <ars_121229@mail.ru>
2022-01-05 08:34:41 +0800

    +маркер

--- 5/Form1.cs
+++ 5/Form1.cs
@@ -6,12 +6,15 @@ namespace _5
     {
         List<BaseObject> objects = new();
         Player player;
+        Marker marker;
         public Form1()
         {
             InitializeComponent();
 
             player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+            marker = new Marker(pbMain.Width / 2+50, pbMain.Height / 2+50, 0);
 
+            objects.Add(marker);
             objects.Add(player);
 
             objects.Add(new MyRectangle(50, 50, 0));
@@ -21,7 +24,7 @@ namespace _5
         private void pbMain_Paint(object sender, PaintEventArgs e)
         {
             var g = e.Graphics;
-            g.Clear(Color.Black);
+            g.Clear(Color.White);
 
             foreach (var obj in objects)
             {
