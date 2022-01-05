1cb90f357c33db00873af487439b28d74a38532c
Арсений Сендецкий <ars_121229@mail.ru>
2022-01-05 08:47:59 +0800

    маркер перемещается по нажатию мыши

--- 5/Form1.cs
+++ 5/Form1.cs
@@ -12,11 +12,11 @@ namespace _5
             InitializeComponent();
 
             player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
-            marker = new Marker(pbMain.Width / 2+50, pbMain.Height / 2+50, 0);
+            marker = new Marker(pbMain.Width / 2 + 50, pbMain.Height / 2 + 50, 0);
 
             objects.Add(marker);
-            objects.Add(player);
 
+            objects.Add(player);
             objects.Add(new MyRectangle(50, 50, 0));
             objects.Add(new MyRectangle(100, 100, 45));
         }
@@ -32,6 +32,27 @@ namespace _5
                 obj.Render(g);
             }
 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float dx = marker.X - player.X;
+            float dy = marker.Y - player.Y;
+
+            float length = MathF.Sqrt(dx*dx + dy*dy);
+            dx /= length;
+            dy /= length;
+
+            player.X += dx * 2;
+            player.Y += dy * 2;
+            
+            pbMain.Invalidate();
+        }
+
+        private void pbMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            marker.X = e.X;
+            marker.Y = e.Y;
         }
     }
 }
